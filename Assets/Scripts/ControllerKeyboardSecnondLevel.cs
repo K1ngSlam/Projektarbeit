59e4ed1c946ec8a604e612d1b3741c82eb554a36
Blackyoshi <johannesschenker762@gmail.com>
2021-11-11 17:11:25 +0100

    NewLevel
    New level and assicitated Scripts for Keyboard

---
+++ Assets/Scripts/ControllerKeyboardSecnondLevel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerKeyboardSecnondLevel : SecondSuper
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<KeyCode> codes = new List<KeyCode>();
+        codes.Add(KeyCode.A);
+        codes.Add(KeyCode.W);
+        codes.Add(KeyCode.S);
+        codes.Add(KeyCode.D);
+        Debug.Log("Keyboard Level 2 Start");
+        Starter("Space", KeyCode.Space, codes);
+    }
+
+    void Update()
+    {
+        Debug.Log("Keyboard Update");
+        Updater();
+    }
+}
--- Assets/Scripts/SecondSuper.cs
+++ Assets/Scripts/SecondSuper.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using System.Linq;
 using UnityEngine.SceneManagement;
 
-public class SuperController : MonoBehaviour
+public class SecondSuper : MonoBehaviour
 {
     [SerializeField]
     protected SpriteRenderer background;
@@ -21,23 +21,31 @@ public class SuperController : MonoBehaviour
     protected List<float> reactionTimeAverage;
     protected short counter;
 
-    protected string _ButtonName;
-    protected KeyCode _usedKey;
+    protected string _StarterButtonName;
+    protected KeyCode _StartKey;
+    protected KeyCode _SearchedKey;
+    protected List<KeyCode> _codes;
 
-    protected void Starter(string ButtonName, KeyCode key)
+
+    protected void Starter(string StarterButtonName, KeyCode key, List<KeyCode> codes)
     {
-        _usedKey = key;
-        if (!ButtonName.Equals(""))
+        _StartKey = key;
+        if (!StarterButtonName.Equals(""))
         {
-            _ButtonName = ButtonName + " ";
+            _StarterButtonName = StarterButtonName + " ";
         }
         red = new Color(231f / 255f, 24f / 255f, 55f / 255f);
         green = new Color(73f / 255f, 182f / 255f, 117f / 255f);
         background.color = red;
+        _codes = codes;
+        int random_key_index = Random.Range(0, codes.Count);
+        _SearchedKey = codes[random_key_index];
+
+
         reactionTime = 0f;
         randomDelay = 0f;
         startTime = 0f;
-        information.text = "Press " + _ButtonName + "to Start!";
+        information.text = "Press " + _StarterButtonName + "to Start!";
         clockisTicking = false;
         timerstopable = false;
         reactionTimeAverage = new List<float>();
@@ -46,7 +54,7 @@ public class SuperController : MonoBehaviour
 
     protected void Updater()
     {
-        if (Input.GetKeyDown(_usedKey))
+        if (Input.GetKeyDown(_StartKey))
         {
             if (!clockisTicking && counter == 3)
             {
@@ -58,12 +66,54 @@ public class SuperController : MonoBehaviour
             }
             else if (!clockisTicking)
             {
+                int random_key_index = Random.Range(0, _codes.Count);
+                _SearchedKey = _codes[random_key_index];
                 StartCoroutine("StartDelay");
                 information.text = "Wait for Green!";
                 background.color = red;
                 clockisTicking = true;
                 timerstopable = false;
             }
+            else if (clockisTicking && timerstopable)
+            {
+                StopCoroutine("StartDelay");
+                int random_key_index = Random.Range(0, _codes.Count);
+                _SearchedKey = _codes[random_key_index];
+                counter++;
+                Debug.Log(counter);
+                reactionTime = Time.time - startTime;
+                reactionTimeAverage.Add(reactionTime);
+                if (counter == 3)
+                {
+                    information.text = "Reaction time:\n" + reactionTime.ToString("N3") + "sec\n Press to see Average";
+                }
+                else
+                {
+                    information.text = "Reaction time:\n" + reactionTime.ToString("N3") + " sec\n Press " + _StarterButtonName + "to start again";
+                }
+                clockisTicking = false;
+            }
+            else if (clockisTicking && !timerstopable)
+            {
+                StopCoroutine("StartDelay");
+                reactionTime = 0f;
+                clockisTicking = false;
+                timerstopable = false;
+                information.text = "Too soon!!\n Press " + _StarterButtonName + "to start again";
+            }
+        }
+
+        if (Input.GetKeyDown(_SearchedKey))
+        {
+            if (!clockisTicking && counter == 3)
+            {
+                information.text = "Test is Over!\n Your Average is: " + reactionTimeAverage.Average().ToString("N3") + "sec";
+                background.color = green;
+                timerstopable = false;
+                SceneManager.LoadScene("Main Menu"); //Zu ladende Scene einfach hier rein
+                //TODO:Speichern des Averages
+            }
+            
             else if (clockisTicking && timerstopable)
             {
                 StopCoroutine("StartDelay");
@@ -77,7 +127,7 @@ public class SuperController : MonoBehaviour
                 }
                 else
                 {
-                    information.text = "Reaction time:\n" + reactionTime.ToString("N3") + " sec\n Press " + _ButtonName + "to start again";
+                    information.text = "Reaction time:\n" + reactionTime.ToString("N3") + " sec\n Press " + _StarterButtonName + "to start again";
                 }
                 clockisTicking = false;
             }
@@ -87,17 +137,20 @@ public class SuperController : MonoBehaviour
                 reactionTime = 0f;
                 clockisTicking = false;
                 timerstopable = false;
-                information.text = "Too soon!!\n Press " + _ButtonName +  "to start again";
+                information.text = "Too soon!!\n Press " + _StarterButtonName + "to start again";
             }
         }
 
     }
+
+
 
     protected IEnumerator StartDelay()
     {
         randomDelay = Random.Range(0.5f, 5f);
         yield return new WaitForSeconds(randomDelay);
         background.color = green;
+        information.text = "Press " + _SearchedKey.ToString() +"!";
         startTime = Time.time;
         clockisTicking = true;
         timerstopable = true;
